dc7f24a45919d2de3069cbe2381f5422211ae912
SebastienBn <seb.aguilar@hotmail.com>
2021-05-07 18:27:22 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+Post.create ([
+    {id:1, user_id:1, title:"Task Manager App Design", image:"https://cdn.dribbble.com/users/1615584/screenshots/15571949/media/7e95f0fddb7957220033569815613b10.jpg?compress=1&resize=1200x900",desciprion:"Mobile App in Glass Design",category_id 5}
+    {id:2, user_id:1, title:"Dark UI Design", image: "https://cdn.dribbble.com/users/1615584/screenshots/15586734/media/bba5187daec75e99477a5055c73c67f4.jpg?compress=1&resize=1200x900",desciprion: "Dark UI Elements design",category_id 5}
+    {id:3, user_id:1, title:"Mountains Illustration", image: "https://cdn.dribbble.com/users/4859/screenshots/15481684/media/288c024c3484513bccec2cc3148de692.png?compress=1&resize=1200x900",desciprion: "Mountains Illustration for a website, using adobe Illustrator",,category_id 5}
+    {id:4, user_id:1, title:"S X letters", image: "https://cdn.dribbble.com/users/250507/screenshots/15605236/media/5a829f37209bee572a6cf4d560df8dee.jpg?compress=1&resize=1200x900",desciprion:"Designing letters for fun" ,category_id 5}
+    {id:5, user_id:1, title:"Credit Card design", image: "https://cdn.dribbble.com/users/32512/screenshots/15319381/media/1b430d053c33635c26ec1186f5f30838.png?compress=1&resize=1200x900",desciprion: "",category_id 7}
+    {id:6, user_id:1, title:"App Model Animation", image: "https://cdn.dribbble.com/users/557720/screenshots/15594807/media/75b8b361c8c6093ec071e17bab7492d3.gif",desciprion: "",category_id 4}
+    {id:7, user_id:1, title:"D - 36th day", image: "https://cdn.dribbble.com/users/1578582/screenshots/15480323/media/cdde019a08960c01e2d8c909c8002376.jpg?compress=1&resize=1200x900",desciprion: "",category_id 9}
+    {id:8, user_id:1, title:"Weekly work - pieces 2020", image: "https://cdn.dribbble.com/users/1578582/screenshots/14825287/media/d3b905f2395edca58cd833e3a6c0b3ae.jpg?compress=1&resize=1200x900",desciprion: "",category_id 5}
+    {id:9, user_id:1, title:"Skygear logo", image: "https://cdn.dribbble.com/users/4859/screenshots/14912530/media/631d88cb7b34acd84051af4bc54cdce9.jpg?compress=1&resize=1200x900",desciprion: "Neural-Network-Enabled Drone Controller",category_id 10}
+    {id:10, user_id:1, title:"WWP°277 \"boundaries\"", image: "https://cdn.dribbble.com/users/1578582/screenshots/14707724/media/3bb7c6d9141559885f8e0c95aa270de2.png?compress=1&resize=1200x900",desciprion: "",category_id 5}
+    {id:11, user_id:1, title:"Addidas Logo", image: "https://cdn.dribbble.com/users/1578582/screenshots/14620621/media/46c807c93a1641957acb7767fc42b8fd.jpg?compress=1&resize=1200x900",desciprion: "",category_id 5}
+    {id:12, user_id:1, title:"Tulips", image: "https://cdn.dribbble.com/users/2220178/screenshots/15604181/media/0be88244b976e55c852b8a49b2925761.jpg?compress=1&resize=1200x900",desciprion: "",category_id 5}
+    {id:13, user_id:1, title:"Fashion Website Design", image: "https://cdn.dribbble.com/users/3005802/screenshots/15600310/media/cac8c26eb0f98302b553b4fbf30ccbbb.png?compress=1&resize=1200x900",desciprion: "",category_id 6}
+    {id:14, user_id:1, title:"Digital Dystopia/Utopia", image: "https://cdn.dribbble.com/users/809349/screenshots/15606712/media/194cc27ad8fbfdea5c9985f9e802a8f5.jpg?compress=1&resize=1200x900",desciprion: ,category_id 5}
+    {id:15, user_id:1, title:"Practice3", image: "https://cdn.dribbble.com/users/1444340/screenshots/15602853/media/9228a05d465595d38c1c6f26e0716d11.jpg?compress=1&resize=1200x900",desciprion:"" ,category_id 5}
+    {id:16, user_id:1, title:"Kohaku – Product Page", image: "https://cdn.dribbble.com/users/427857/screenshots/15613973/media/7c696593068ab9fac6b468bd40370e4f.png?compress=1&resize=1200x900",desciprion: "",category_id 6}
+])
